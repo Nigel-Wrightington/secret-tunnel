b2d9ee3142d6106440d33c0ae7299a3a0a7e5ef3
nigelwrightington <nigelwrightington@yahoo.com>
2025-09-24 22:28:16 -0400

    full project submitted

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -10,9 +10,39 @@ export function AuthProvider({ children }) {
 
   // TODO: signup
 
+  //I need to send credentials to API to get a token
+  const signup = async (credentials) => {
+    try {
+      const response = await fetch(`${API}/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      });
+      const result = await response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  };
+
   // TODO: authenticate
 
-  const value = { location };
+  //send the token to the API to authenticate the user
+  const authenticate = async () => {
+    try {
+      if (!token) throw Error("No token found for authentication");
+      const response = await fetch(`${API}/authenticate`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (!response.ok) throw Error("Authentication failed");
+      setLocation("TUNNEL");
+    } catch (error) {
+      console.error("Error during authentication:", error);
+    }
+  };
+
+  const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
